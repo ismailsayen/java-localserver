6667fb565ca9072f358d5b64302703216a01dfe5
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-03-20 16:16:36 +0000

    reading the file and check if the script exists

--- src/handlers/CgiHandler.java
+++ src/handlers/CgiHandler.java
@@ -1,9 +1,11 @@
 package handlers;
 
+import java.io.File;
 import java.io.IOException;
 
 import Nio.ClientHandler;
 import http.HttpHandler;
+import http.HttpRequest;
 
 public class CgiHandler implements HttpHandler {
 
@@ -16,7 +18,16 @@ public class CgiHandler implements HttpHandler {
 
     @Override
     public void handle() throws Exception {
-        
+        HttpRequest request = client.getHttpRequest();
+
+        String method = client.getHttpHeader().getMethod();
+        String scriptPath = request.resolveFilePath();
+
+        File script = new File(scriptPath);
+        if (!script.exists()) {
+            responseBytes = "<h1>404 Not Found</h1>".getBytes();
+            return;
+        }
     }
 
     @Override
